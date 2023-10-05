c6bb54221d0e3c73451a07b0971353c5058894f6
tranceblastr <brian.andrais@gmail.com>
2023-10-05 18:14:28 -0300

    cart controller incompletos

--- Server/src/controllers/cartController.js
+++ Server/src/controllers/cartController.js
@@ -1,5 +1,50 @@
-let cart = [];
+const { User, Cart } = require("../database/db");
+const { Op } = require("sequelize");
 
-const getAllItems = () => {
-  
+
+const  getUserCartController = async (userId) => {
+try {
+  const userCart = Cart.findAll({where:{userId}})
+  return userCart
+} catch (error) {
+console.log(error)  
+throw error
+}
+}
+
+const  addItemToCartController = async (userId, bookId) => {
+try {
+  //add bookid where userid
+} catch (error) {
+console.log(error)  
+throw error
+}
+}
+
+const  updateBookQuantityController = async (userId, bookId, quantity) => {
+try {
+  //patch quantity where userid & bookid
+} catch (error) {
+console.log(error)  
+throw error
+}
+}
+
+const  deleteItemFromCartController = async (userId, bookId) => {
+try {
+  //delete where userid & bookid
+} catch (error) {
+console.log(error)  
+throw error
+}
+}
+
+
+
+
+module.exports ={
+getUserCartController,
+addItemToCartController,
+updateBookQuantityController,
+deleteItemFromCartController
 }
