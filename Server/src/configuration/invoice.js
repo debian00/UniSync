ac640174e6367cc1900f753de28929fdc2a6088f
Soynexoo <Soynexoo@example.com>
2023-10-14 23:48:25 -0300

    INVOICE

--- Server/src/configuration/invoice.js
+++ Server/src/configuration/invoice.js
@@ -1,4 +1,4 @@
-const easyinvoice = require('easyinvoice');
+const easyinvoice = require("easyinvoice");
 const { User } = require("../database/db");
 
 const generateInvoice = async (req, res) => {
@@ -7,14 +7,14 @@ const generateInvoice = async (req, res) => {
     const { id } = req.params;
 
     const user = await User.findOne({ where: { id: id } });
-    
+
     if (!user) {
-      return res.status(404).json({ error: 'Usuario no encontrado' });
+      return res.status(404).json({ error: "Usuario no encontrado" });
     }
 
     const itemsBought = items.map((elem) => {
       return {
-        item: elem.title,
+        item: elem.book.title,
         description: elem.book.author,
         quantity: elem.quantity,
         unitPrice: elem.price,
@@ -22,45 +22,53 @@ const generateInvoice = async (req, res) => {
     });
 
     const currentDate = new Date();
-    const formattedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)
+    const formattedDate = `${currentDate.getFullYear()}-${(
+      currentDate.getMonth() + 1
+    )
       .toString()
-      .padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+      .padStart(2, "0")}-${currentDate.getDate().toString().padStart(2, "0")}`;
 
     const data = {
-      documentTitle: 'Factura',
-      currency: 'USD',
-      taxNotation: 'GST',
+      documentTitle: "Factura",
+      currency: "USD",
+      taxNotation: "vat",
       marginRight: 50,
       marginLeft: 50,
       marginTop: 25,
       marginBottom: 25,
-      logo: 'https://www.example.com/logo.png',
+      logo: "https://www.example.com/logo.png",
       sender: {
-        company: 'The-Next-Page-Library',
-        address: 'Greed Island',
-        zip: '12345',
-        city: 'Ciudad del Pecado',
-        country: 'Dubai',
+        company: "The-Next-Page-Library",
+        address: "Greed Island",
+        zip: "12345",
+        city: "Ciudad del Pecado",
+        country: "Dubai",
       },
       client: {
         name: user.name,
         email: user.email,
-        phone: user.phoneNumber
+        phone: user.phoneNumber,
       },
-      invoiceNumber: '2023-001',
+      invoiceNumber: "2023-001",
       invoiceDate: formattedDate,
-      products: itemsBought,
-      bottomNotice: 'Gracias por su compra.',
+      products: [
+        {
+          description: "llibrito",
+          quantity: 2,
+          price: 400,
+        },
+      ],
+      bottomNotice: "Gracias por su compra.",
     };
 
-    const result = easyinvoice.createInvoice(data);
+    const result = await easyinvoice.createInvoice(data);
 
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename=factura.pdf');
-    res.send(Buffer.from(result.pdf, 'base64'));
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "attachment; filename=factura.pdf");
+    res.send(Buffer.from(result.pdf, "base64"));
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error al generar factura' });
+    res.status(500).json({ error: "Error al generar factura" });
   }
 };
 
