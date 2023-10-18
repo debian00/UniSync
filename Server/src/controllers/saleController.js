915502e1ad6193fbf5bae9cb2d0364d364d8d1ab
OsKing <oscar00gaona@gmail.com>
2023-10-18 16:47:04 -0500

    Envio email de ventas

--- Server/src/controllers/saleController.js
+++ Server/src/controllers/saleController.js
@@ -1,6 +1,90 @@
-const { Sale, Book } = require("../database/db");
+const { Sale, Book, User} = require("../database/db");
 const Sequelize = require("sequelize");
 const { Op } = require("sequelize");
+const nodemailer = require("nodemailer");
+const path = require('path');
+const fs = require('fs'); // Asegúrate de requerir fs
+const templatePath = path.join(__dirname, '../configuration/mailingTemplates/Comprar Libro', 'index.html');
+
+//Crea una nueva venta
+const createSaleController = async ({
+  userId,
+  bookId,
+  purchaseDate,
+  totalPrice,
+  quantity,
+  paymentMethod,
+  cartId,
+}) => {
+  try {
+    const newSale = await Sale.create({
+      userId,
+      bookId,
+      purchaseDate,
+      totalPrice,
+      quantity,
+      paymentMethod,
+      cartId,
+    });
+
+    const userid = await User.findByPk(userId);
+    const bookid = await Book.findByPk(bookId)
+    const emailuser = userid.email
+    console.log('user', emailuser);
+    console.log('libro', bookid);
+
+    fs.readFile(templatePath, 'utf8', async (err, html) => {
+      if (err) {
+        console.error('Error al leer el archivo de plantilla:', err);
+        return res.status(500).json({ error: "Error al registrar el usuario", details: err.message });
+      }
+      var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465, // Cambiado de "post" a "port"
+        secure: true,
+        auth: {
+          user: "greattravel.contact@gmail.com",
+          pass: "hbacczxxirmcjmht"
+        },
+        tls:{
+          rejectUnauthorized:false
+        }
+      });
+      html = html.replace('<p id="fechaVenta"></p>', `<p id="fechaVenta">${purchaseDate}</p>`);
+      html = html.replace('{IMAGE_SRC}', bookid.images[0]);
+      html = html.replace('<strong id="nombreLibroVenta"></strong>', `<strong id="nombreLibroVenta">${bookid.title}</strong>`);
+      html = html.replace('<p id="autorLibroVenta"></p>', `<p id="autorLibroVenta">${bookid.author}</p>`);
+      html = html.replace('<p id="cantidadLibroVenta"></p>', `<p id="cantidadLibroVenta">Cantidad: ${quantity}</p>`);
+      html = html.replace('<p id="precioLibroVenta">$</p>', `<p id="precioLibroVenta">$ ${bookid.sellPrice}</p>`);
+      html = html.replace('<p id="precioTotalLibroVenta">Total:</p>', `<p id="precioTotalLibroVenta">Total: ${totalPrice}</p>`);
+      
+      // html = html.replace('<strong id="userPassword"></strong>', `<strong id="userPassword">${text}</strong>`);
+      // html = html.replace('<img id="imagenLibroVenta" class="adapt-img" src="" alt style="display: block;" width="70" />', `<img id="imagenLibroVenta" class="adapt-img" src=${bookid.images[0]} alt style="display: block;" width="70" />`);
+       // Detalles del correo
+       const mailOptions = {
+        from: 'greattravel.contact@gmail.com',
+        to: emailuser, // Utiliza la dirección del destinatario proporcionada en el cuerpo de la solicitud
+        subject:"Datos de tu compra!",
+        html:html, // Puedes cambiar esto a HTML si lo deseas
+      };
+  
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          res.status(500).send(error.message);
+        } else {
+          console.log("Email Enviado")
+          res.status(200).jsonp(req.body);
+        }
+      });
+    }) 
+
+
+    return newSale;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 //Trae todas las ventas
 const getAllSaleController = async () => {
@@ -56,32 +140,7 @@ const getSaleByUserIdController = async (userId) => {
     throw error;
   }
 };
-//Crea una nueva venta
-const createSaleController = async ({
-  userId,
-  bookId,
-  purchaseDate,
-  totalPrice,
-  quantity,
-  paymentMethod,
-  cartId,
-}) => {
-  try {
-    const newSale = await Sale.create({
-      userId,
-      bookId,
-      purchaseDate,
-      totalPrice,
-      quantity,
-      paymentMethod,
-      cartId,
-    });
-    return newSale;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
+
 
 //Modifica una venta
 const updateSaleController = async (
