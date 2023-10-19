35b1cf9a49370c5609001715e9a98a51b519f020
Julian Luque <Soynexoo@gmail.com>
2023-10-18 23:00:56 -0300

    options

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,44 +1,46 @@
-require('dotenv').config();
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const session = require("express-session");
-const pgSession = require('connect-pg-simple')(session);
-const { Pool } = require('pg')
+const pgSession = require("connect-pg-simple")(session);
+const { Pool } = require("pg");
 const passport = require("passport");
-const router = require('./routes/index')
+const router = require("./routes/index");
 
 //Server levantado
-const server = express()
+const server = express();
 
-server.use(morgan('dev'));
-server.use(express.json())
-server.use(cors({
-  origin:'https://the-next-page.vercel.app',
-  credentials: true,
-}))
+server.use(morgan("dev"));
+server.use(express.json());
+server.use(
+  cors({
+    origin: "https://the-next-page.vercel.app",
+    credentials: true,
+  })
+);
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: 'railway',
+  database: "railway",
   password: process.env.DB_PASSWORD,
-  port: 5432
+  port: 5432,
 });
 
-
+server.options("*", cors());
 //session express
 server.use(
   session({
     store: new pgSession({
       pool,
-      tableName: 'session',
-      createTableIfMissing: true
+      tableName: "session",
+      createTableIfMissing: true,
     }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 días
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 días
     // Inserta otras opciones de express-session según sea necesario
   })
 );
@@ -55,13 +57,12 @@ passport.deserializeUser((id, cb) => {
 });
 
 //Enlace a los endpoints
-server.use(router)
-
+server.use(router);
 
 //Error por si algo falla por aca:
 server.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json('Error interno del servidor...')
-})
+  res.status(500).json("Error interno del servidor...");
+});
 
-module.exports = server
+module.exports = server;
